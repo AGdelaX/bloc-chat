da7aab9cf5c56a873b51db43139c4d2a228d75f8
AGdelaX <agdelax@gmail.com>
2015-05-31 13:10:05 -0400

    Added a text input to add rooms

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,16 +1,77 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-blocChat = angular.module('BlocChat', ['ui.router', 'firebase']);
+"use strict";
 
-blocChat.factory('Room', ['$firebase', function($firebase){
+var blocChat = angular.module('BlocChat', ['ui.router', 'firebase']);
+										//NOTE: I removed ui.bootstrap
+blocChat.factory('Room', ['$firebaseArray', function($firebaseArray){
 	var firebaseRef = new Firebase('https://shining-torch-9429.firebaseio.com/');
-	var rooms = $firebase(firebaseRef.child('rooms')).$asArray();;
+	var rooms = $firebaseArray(firebaseRef.child('rooms'));
+
+	// rooms.$add({name: 'Room 1'});
 
 	return {
 		all: rooms
 	};
 }]);
 
-blocChat.controller('RoomsDisplay.controller', ['$scope', 'Room', function($scope, Room){
+blocChat.controller('RoomsDisplay', ['$scope', 'Room', function($scope, Room){
 	$scope.rooms = Room.all;
 }]);
+
+blocChat.controller('RoomsForm', ['$scope', 'Room', function($scope, Room) {
+	$scope.form = {};
+
+	var rooms = Room.all;
+
+	$scope.click = function(){
+		rooms.$add($scope.form);
+		console.log(Room.all);
+	};
+}]);
+
+// blocChat.directive('inputform', function(){
+// 	return {
+// 		templateUrl: '../templates/directives/inputform.html',
+// 		replace: true,
+// 		restrict: 'E',
+// 		link: function(scope, element, attributes) {
+
+// 			var $form = $(element);
+
+
+// 		};
+
+// 	};
+// });
+
+// blocChat.controller('modalWindow', ['$scope', '$modal', 'Room', '$log', function($scope, $modal, Room, $log) {
+
+// 	$scope.open = function() {
+
+// 		var modalInstance = $modal.open({
+
+// 		templateUrl: '../templates/modal.html',
+// 		controller: 'modalInstanceCtrl'
+
+
+// 		});
+// 	};
+// }]);
+
+// blocChat.controller('modalInstanceCtrl', ["$scope", "$modalInstance", "Room", "form", function($scope, $modalInstance, Room, form) {
+
+// 		$scope.form = {
+// 		text: " "
+// 		};
+
+// 	$scope.input = function() {
+// 		rooms.$add({name: $scope.form.text});
+// 		$modalInstance.close($scope.form.text);
+// 	};
+
+// 	$scope.cancel= function(){
+// 		$modalInstance.dismisss('cancel');
+// 	};
+// }]);
+
 },{}]},{},[1]);
